31bd71787edbe2827af7d633be1685b32cd2a5b1
vasurya6_hub <vasurya6@gmail.com>
2026-05-31 14:53:34 +0530

    python emc -1

---
+++ python basic.py
@@ -0,0 +1,23 @@
+
+a=10
+b=20
+c=(a+b)
+#print(c)
+#print(c)
+#my_list = (1,2,3,4)
+#print(my_list[3])
+#my_age = input(27)
+#print(my_age)
+#age = input(25)
+#print(age)
+#print(my_age)
+#for val in my_age:
+     #print(val)
+#for val in my_list:
+    #print(val)
+age = input("eligible age")
+if age == "18":
+    print("you are an adult")
+else:    
+    print("minor")
+    
